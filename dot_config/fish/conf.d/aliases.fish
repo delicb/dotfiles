7870bd6aecb3149c0d78ae8c2d5330ae323931d5
Bojan Delić <bojan@delic.rs>
2021-07-23 14:53:19 +0200

    add fish aliases

--- dot_config/fish/conf.d/aliases.fish
+++ dot_config/fish/conf.d/aliases.fish
@@ -1,6 +1,28 @@
-    # fish does not really have aliases, but alias function that generates
-    # other functions and marks them as wrapper for original commands (so that
-    # completions will work)
-    alias l="exa -lb --git --color-scale"
-    alias g="git"
-    alias gss="git status"
+# fish does not really have aliases, but alias function that generates
+# other functions and marks them as wrapper for original commands (so that
+# completions will work)
+
+# common aliases
+alias .. "cd .."
+alias ... "cd ../.."
+alias .... "cd ../../.."
+alias ls exa
+alias l "exa -lb --git --color-scale"
+alias ll l
+alias la "l -a"
+
+# suffixes for common commands
+alias cp "cp -i"
+alias df "df -h"
+alias du "du -sh"
+
+# git aliases
+alias g git
+alias ga "git add"
+alias gd "git diff"
+alias gf "git fetch"
+alias gp "git pull"
+alias gss "git s"
+
+# reload config
+alias reload "exec $SHELL -l"
