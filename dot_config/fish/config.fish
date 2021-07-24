558c9b2f7371649989cef6379c1ad232d2852a75
Bojan Delić <bojan@delic.rs>
2021-07-24 20:33:06 +0200

    use starship for prompt for fish, remove fisher since it is not used now

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,16 +1,21 @@
 if status is-interactive
-    
-    set fisher_path $HOME/.config/fish/plugins
+    # set fisher_path $HOME/.config/fish/plugins
 
-    set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
-    set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
+    # set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..-1]
+    # set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..-1]
 
-    for file in $fisher_path/conf.d/*.fish
-        source $file
+    # for file in $fisher_path/conf.d/*.fish
+    #     source $file
+    # end
+
+    # initialize brew environment variables
+    if command -q brew
+        brew shellenv | source
     end
-    # Commands to run in interactive sessions can go here
-    # starship init fish | source
 
-    # direnv hook fish | source
+    # add asdf executable and shims to path
     set PATH $HOME/.asdf/bin $HOME/.asdf/shims $PATH
+
+    # initialize prompt
+    starship init fish | source
 end
