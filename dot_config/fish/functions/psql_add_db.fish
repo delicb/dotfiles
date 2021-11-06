71c9dcfd38a6160bd95ce16aec097885cce3b958
Bojan Delić <bojan@delic.rs>
2021-11-07 00:13:59 +0100

    set default password for psql_add_db command

--- dot_config/fish/functions/psql_add_db.fish
+++ dot_config/fish/functions/psql_add_db.fish
@@ -1,5 +1,5 @@
 function psql_add_db --description "creates new DB on existing instance with new user"
-	argparse --name='psql_add_db' 'help' "h/host=" "p/port=" "U/username=" "W/password=" "u/existing-user=" -- $argv
+	argparse --name='psql_add_db' 'help' "h/host=" "p/port=" "U/username=" "W/password=" "u/existing-user=" "P/existing-password" -- $argv
 	or return
 
 	if set --query _flag_help
@@ -8,11 +8,12 @@ function psql_add_db --description "creates new DB on existing instance with new
 		printf "for existing user that has permissions to create new database and user\n\n"
 		printf "Options:\n"
 		printf "  --help  prints help and exists\n"
-		printf "  -h/--host           hostname where PSQL instance is, default 'localhost'\n"
-		printf "  -p/--port           port where PSQL listening on, default '5432'\n"
-		printf "  -U/--username       username to create, default the same as database name\n"
-		printf "  -W/--password       password to set for user, default 'secret'\n"
-		printf "  -u/--existing-user  userame for user that already exists and has permissions to create DB, default 'postgres'"
+		printf "  -h/--host              hostname where PSQL instance is, default 'localhost'\n"
+		printf "  -p/--port              port where PSQL listening on, default '5432'\n"
+		printf "  -U/--username          username to create, default the same as database name\n"
+		printf "  -W/--password          password to set for user, default 'secret'\n"
+		printf "  -u/--existing-user     userame for user that already exists and has permissions to create DB, default 'postgres'"
+		printf "  -P/--existing-password passwor for user that already exists and has permissions to create DB, default 'password'"
 		return 0
 	end
 
@@ -30,6 +31,7 @@ function psql_add_db --description "creates new DB on existing instance with new
 	set --query _flag_username; or set --local _flag_username $_db_name
 	set --query _flag_password; or set --local _flag_password 'secret'
 	set --query _flag_existing_user; or set --local _flag_existing_user 'postgres'
+	set --query _flag_existing_password; or set --local _flag_existing_password 'password'
 
 	# check if psql command is available
 	if not command -sq psql
@@ -37,5 +39,5 @@ function psql_add_db --description "creates new DB on existing instance with new
 		return 1
 	end
 
-	printf "\set AUTOCOMMIT on\n create database $_db_name; \n create user $_flag_username with encrypted password '$_flag_password'; \n grant all privileges on database $_db_name to $_flag_username;" | psql -h $_flag_host -U $_flag_existing_user
+	printf "\set AUTOCOMMIT on\n create database $_db_name; \n create user $_flag_username with encrypted password '$_flag_password'; \n grant all privileges on database $_db_name to $_flag_username;" | PGPASSWORD=$_flag_existing_password psql -h $_flag_host -U $_flag_existing_user
 end
