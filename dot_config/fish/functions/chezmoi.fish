748caac6e8c0917dabb282b0628f7b641907b8df
Bojan Delić <bojan@delic.rs>
2021-12-08 00:28:31 +0100

    add cat to chezmoi as one needing credentials

--- dot_config/fish/functions/chezmoi.fish
+++ dot_config/fish/functions/chezmoi.fish
@@ -2,7 +2,7 @@ function chezmoi --wraps=chezmoi --description="Wrapper for chezmoi that automat
 	# a lot of chezmoi commands do not require 1password, so detect just the
 	# ones that do and force login
 	# this is probably not a full list, so add new items if it is needed
-	for cmd in "apply" "diff" "execute_template" "secret" "update"
+	for cmd in "apply" "diff" "execute_template" "secret" "update" "cat"
 		if contains $cmd $argv
 			if command -q op
 				op_signin
