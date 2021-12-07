27108f3978c004c27fb6d29ddaf71b9369931886
Bojan Delić <bojan@delic.rs>
2021-12-07 22:17:18 +0100

    chezmoi command - only login to 1password if subcommand requires it

--- dot_config/fish/functions/chezmoi.fish
+++ dot_config/fish/functions/chezmoi.fish
@@ -1,6 +1,17 @@
 function chezmoi --wraps=chezmoi --description="Wrapper for chezmoi that automatically logs in to 1password CLI"
-	if command -q op
-		op_signin
+	# a lot of chezmoi commands do not require 1password, so detect just the
+	# ones that do and force login
+	# this is probably not a full list, so add new items if it is needed
+	for cmd in "apply" "diff" "execute_template" "secret" "update"
+		if contains $cmd $argv
+			if command -q op
+				op_signin
+			end
+			OP_SESSION_my=(cat $HOME/.op_session) command chezmoi $argv
+			return
+		end
 	end
-	OP_SESSION_my=(cat $HOME/.op_session) command chezmoi $argv
+
+	# otherwise just execute chezmoi
+	command chezmoi $argv
 end
