a391317a3b680be3b296e9d0b6806489524dd400
Bojan Delić <bojan@delic.rs>
2022-08-25 13:51:49 +0200

    add kubectl command with automatic login

---
+++ dot_config/fish/functions/kubectl.fish
@@ -0,0 +1,9 @@
+function kubectl --wraps=kubectl --description "Wrapper for kubectl that does automatic signin if needed"
+	# if "org" command exists, use it to login in to pip
+	# org command is smart enough not to login if credentials are
+	# still valid, so it is not that expensive to call it every time
+	if command -q org
+		command org aws login --quiet
+	end
+	command kubectl $argv
+end
