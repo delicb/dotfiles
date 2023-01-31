3b7e9ee0f8cfc0834c2caae6bdb2d168b2a55354
Bojan Delić <bojan@delic.rs>
2023-01-31 22:52:54 +0100

    separte dev config to its own file

--- dot_config/fish/conf.d/10-basic.fish
+++ dot_config/fish/conf.d/10-basic.fish
@@ -8,11 +8,6 @@ set -gx EDITOR "vim"
 set -gx PAGER "less"
 set -gx LESS "--RAW-CONTROL-CHARS --quit-if-one-screen"
 
-# golang
-set -gx GOPATH $HOME/go
-
-# disable SAM CLI telemetry
-set -gx SAM_CLI_TELEMETRY 0
 
 # delate pager, for page-up-down to work in VSCode (since delta is not respecting $LESS variable)
 set -gx DELTA_PAGER "less --RAW-CONTROL-CHARS --quit-if-one-screen"
@@ -20,10 +15,3 @@ set -gx DELTA_PAGER "less --RAW-CONTROL-CHARS --quit-if-one-screen"
 # set ripgrep config file
 set -gx RIPGREP_CONFIG_PATH "$HOME/.ripgreprc"
 
-# pipx config
-set -gx PIPX_DEFAULT_PYTHON (rtx where python)/bin/python
-set -gx USE_EMOJI true
-
-# require virtualenv for pip
-# to install to non-venv, use 'PIP_REQUIRE_VIRTUALENV="" pip install ...'
-set -gx PIP_REQUIRE_VIRTUALENV true 
---
+++ dot_config/fish/conf.d/50-dev.fish
@@ -0,0 +1,15 @@
+# general development related configs
+
+# golang
+set -gx GOPATH $HOME/go
+
+# disable SAM CLI telemetry
+set -gx SAM_CLI_TELEMETRY 0
+
+# pipx config
+set -gx PIPX_DEFAULT_PYTHON (rtx where python)/bin/python
+set -gx USE_EMOJI true
+
+# require virtualenv for pip
+# to install to non-venv, use 'PIP_REQUIRE_VIRTUALENV="" pip install ...'
+set -gx PIP_REQUIRE_VIRTUALENV true
