b45f04b7dc366e89b648f2f6c3a4b52b385f1c1d
Bojan Delić <bojan@delic.rs>
2023-10-31 13:16:55 +0100

    a bit smarter cat function parameter handling

--- dot_config/fish/functions/cat.fish
+++ dot_config/fish/functions/cat.fish
@@ -1,7 +1,16 @@
 function cat --wraps=bat --description "Replaces cat with bat, except for markdown, where it uses mdcat"
-	if string match --quiet '*.md' $argv && command --query mdcat
-		mdcat $argv
+	set -l flags
+	set -l params
+	for f in $argv
+		if string match --quiet -- '-*' $f
+			set -a flags $f
+		else
+			set -a params $f
+		end
+	end
+	if string match --quiet '*.md' $params && command --query mdcat
+		mdcat $flags $params
 		return
 	end
-	command bat $argv
+	command bat $flags $params
 end
