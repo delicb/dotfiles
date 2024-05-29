e789ec24a1e6305417e1ccd8f0ad0908d358c049
Bojan Delić <bojan@delic.in.rs>
2024-05-30 01:09:59 +0200

    add fenv function

---
+++ home/dot_config/fish/functions/fenv.fish
@@ -0,0 +1,20 @@
+function fenv --description "print value of selected environment variable"
+	if not type -q fzf; 
+		echo "fzf not installed"
+		return 1
+	end
+
+	set -l var (
+		printenv | 
+		cut -d= -f1 | 
+		fzf --ignore-case \
+			--height=25% \
+			--cycle \
+			--preview='printenv {}' \
+			--preview-window=right:70% \
+			--padding 1 \
+			--preview-label 'Env var value: {}'
+	)
+
+	printenv $var
+end
