2c525dab2f1148fcaa258750a865b7c2f97e0cc4
Bojan Delić <bojan@delic.in.rs>
2026-09-11 13:14:11 +0200

    chore(chezmoi): sync live configuration

--- home/dot_config/fish/conf.d/30-path.fish
+++ home/dot_config/fish/conf.d/30-path.fish
@@ -3,11 +3,8 @@ fish_add_path --path --prepend --move $HOME/.local/bin $HOME/go/bin $HOME/bin
 
 # ensure system paths are there and that they are at the end
 if test -e /etc/paths
-	fish_add_path --path --move --append (cat /etc/paths)  # this basically simulates /usr/libexec/path_helper
+    fish_add_path --path --move --append (cat /etc/paths) # this basically simulates /usr/libexec/path_helper
 else
-	# if there is no /etc/paths, hardcode some common paths
-	fish_add_path --path --move --append /usr/local/bin /usr/bin /bin /usr/sbin /sbin
+    # if there is no /etc/paths, hardcode some common paths
+    fish_add_path --path --move --append /usr/local/bin /usr/bin /bin /usr/sbin /sbin
 end
-
-# linear needs pg_dump 18.3
-fish_add_path --prepend (brew --prefix "$tap/libpq@18.3")/bin
